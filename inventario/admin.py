ac104885a671114d8bac30189196b91dcb644fcd
yulisahiguita1705 <yulisahiguita1705@gmail.com>
2022-11-08 19:35:26 -0500

    Modulo admin

--- inventario/admin.py
+++ inventario/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from inventario.models import Productos
+from import_export.admin import ExportActionMixin
 
 # Register your models here.
+class ProductosAdmin(ExportActionMixin, admin.ModelAdmin):
+
+    list_display = ('name', 'cost')
+    search_fields = ('name', )
+    list_filter  = ('category', 'cantidad_stock')
+    
+
+
+admin.site.register(Productos,ProductosAdmin)
